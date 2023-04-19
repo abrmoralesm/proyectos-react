3d0992ba15d3a68a7dfaffe52318f36605fc54f7
abrmoralesm <abrmoralesm@gmail.com>
2023-04-19 18:38:37 +0200

    mas cambios

--- projects/buscador-gifs/src/App.js
+++ projects/buscador-gifs/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs";
+import { Link, Route } from "wouter"
+import SignUpBanner from "./components/SignUpBanner";
+
+
 
 function App() {
   return (
     <div className="App">
+      <SignUpBanner/>
       <section className="App-content">
-        <ListOfGifs keyword='akira' />
+        <h1>Jar</h1>
+        <Link to="/gif/akira">Gifs de Akira</Link>
+        <Link to="/gif/eevee">Gifs de Eevee</Link>
+        <Link to="/gif/pikachu">Gifs de Pikachu</Link>
+
+        <Route component={ListOfGifs} path="/gif/:keyword" />
       </section>
     </div>
   );
--- projects/buscador-gifs/src/components/ListOfGifs.js
+++ projects/buscador-gifs/src/components/ListOfGifs.js
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
-  const [gifs, setGifs] = useState([]);
+export default function ListOfGifs({ params }) {
+  const { keyword } = params
+  const [loading, setLoading] = useState(false);
+  const [gifs, setGifs] = useState([])
 
-  useEffect(function () {
-    getGifs({ keyword})
-    .then(gifs => setGifs(gifs));
-    //console.log(gifs
-    //setGifs(DIFERENT_GIFS)
-  }, [keyword]);
+  useEffect(function (){
+    setLoading(true)
+    getGifs({keyword})
+    .then(gifs=>{
+    setGifs(gifs)
+    setLoading(false)
+    })
+  },[keyword])
 
-  return gifs.map(({ id, title, type, url }) => (
-    <Gif key={id} type={type} id={id} title={title} url={url} />
-  ));
+  if(loading) return <i> 😼 </i>
+
+  return (
+    <div>
+      {gifs.map(({ id, title, type, url }) => (
+        <Gif key={id} type={type} id={id} title={title} url={url} />
+      ))}
+    </div>
+  );
 }
